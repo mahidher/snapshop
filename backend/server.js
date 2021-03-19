505c10c67a021d889850dba69e04e85c6b7ab14b
mahidher <dkmahidher@gmail.com>
2021-03-19 14:07:30 +0530

    PrepaRE FOR DEPLOYMENT 2

--- backend/server.js
+++ backend/server.js
@@ -62,6 +62,6 @@ app.use(errorHandler);
 // });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
 });
